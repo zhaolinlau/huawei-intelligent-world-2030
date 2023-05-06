545a81a5f0cd1ce633abc140b7999de363b85ae8
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-07 06:03:39 +0800

    build production

---
+++ docs/server/chunks/app/styles.mjs
@@ -0,0 +1,10 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "oruga.js": () => import('./_nuxt/oruga-styles.a38adfe9.mjs').then(interopDefault),
+  "pages/index.vue": () => import('./_nuxt/index-styles.fb47b3c9.mjs').then(interopDefault),
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.80478c74.mjs').then(interopDefault),
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.2483f26f.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
