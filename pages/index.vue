57fda5791c002e7f4d35736f09a95864ae2792b6
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-08 04:18:36 +0800

    draft the rest of section

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,29 @@ const scrollToElement = (id, duration) => {
 		duration: 500,
 	});
 };
+
+const speakers = [
+	{
+		image: "https://images.pexels.com/photos/8348463/pexels-photo-8348463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Sharifah Diana Shima Binti Syed Badrol",
+		position: "Public Affairs and Communications Manager"
+	},
+	{
+		image: "https://images.pexels.com/photos/8348463/pexels-photo-8348463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Ms Carolyn Wai Kar Yin",
+		position: "System Integration & Learning Consultant"
+	},
+	{
+		image: "https://images.pexels.com/photos/8348463/pexels-photo-8348463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Mr. Lai Teik Nan",
+		position: "Data Center Solution Manager"
+	},
+	{
+		image: "https://images.pexels.com/photos/8348463/pexels-photo-8348463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		name: "Ms Yong Sze Miin",
+		position: "Human Resource Manager of Asia Pacific Delivery & Service Dept"
+	}
+]
 </script>
 
 <template>
@@ -28,9 +51,9 @@ const scrollToElement = (id, duration) => {
 							<span class="px-3 has-text-primary has-background-white" style="border-radius: 5px;">Intelligent</span>
 							World 2030
 						</p>
-						<p class="title is-5 has-text-weight-bold has-text-white">9th May 2023, 9 am to 5 pm, Auditorium Library &
+						<p class="title is-5 has-text-weight-bold has-text-white">9th & 10th May 2023, Auditorium Library &
 							Pusat
-							Akademik Pelajar,
+							Aktiviti Pelajar G Floor,
 							UMP
 							Pekan</p>
 					</div>
@@ -72,14 +95,285 @@ const scrollToElement = (id, duration) => {
 					<div class="column is-3">
 						<p class="title is-5 has-text-white">WHERE</p>
 						<p>
-							Auditorium Library & Pusat Akademik Pelajar, UMP Pekan
+							Auditorium Library & Pusat Aktiviti Pelajar G Floor, UMP Pekan
 						</p>
 					</div>
 					<div class="column is-3">
 						<p class="title is-5 has-text-white">WHEN</p>
 						<p>
-							9th May 2023, 9 am to 5 pm
+							9th May 2023 (9 am to 5 pm) <br>
+							10th May 2023 (10 am to 5 pm)
 						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="hero" id="speakers">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-multiline">
+					<div class="column is-12 has-text-centered">
+						<p class="title">EVENT SPEAKERS</p>
+						<p class="subtitle">Here are our speakers</p>
+					</div>
+					<div class="column is-3-desktop is-6-tablet is-12-mobile" v-for="(speaker, i) in speakers" :key="i">
+						<div class="card">
+							<div class="card-image">
+								<figure class="image">
+									<img :src="speaker.image" alt="{{ speaker.image }}">
+								</figure>
+							</div>
+							<div class="card-content">
+								<p class="title is-4">{{ speaker.name }}</p>
+								<p class="subtitle is-6 has-text-primary is-italic">{{ speaker.position }}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="hero has-background-primary-light" id="schedule">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-multiline">
+					<div class="column is-12 has-text-centered">
+						<p class="title">EVENT SCHEDULE</p>
+						<p class="subtitle">Here is our event schedule</p>
+					</div>
+					<div class="column is-12">
+						<div class="columns is-multiline">
+							<div class="column is-6-desktop is-12">
+								<p class="title is-5 has-text-centered">9th May 2023</p>
+								<div class="list">
+									<div class="list-item box">
+										<div class="list-item-content">
+											<div class="list-item-title">List item</div>
+											<div class="list-item-description">List item description</div>
+										</div>
+									</div>
+									<div class="list-item box">
+										<div class="list-item-content">
+											<div class="list-item-title">List item</div>
+											<div class="list-item-description">List item description</div>
+										</div>
+									</div>
+									<div class="list-item box">
+										<div class="list-item-content">
+											<div class="list-item-title">List item</div>
+											<div class="list-item-description">List item description</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="column is-6">
+								<p class="title is-5 has-text-centered">10th May 2023</p>
+								<div class="list">
+									<div class="list-item box">
+										<div class="list-item-content">
+											<div class="list-item-title">List item</div>
+											<div class="list-item-description">List item description</div>
+										</div>
+									</div>
+									<div class="list-item box">
+										<div class="list-item-content">
+											<div class="list-item-title">List item</div>
+											<div class="list-item-description">List item description</div>
+										</div>
+									</div>
+									<div class="list-item box">
+										<div class="list-item-content">
+											<div class="list-item-title">List item</div>
+											<div class="list-item-description">List item description</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="hero" id="venue">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns">
+					<div class="column is-12 has-text-centered">
+						<p class="title">EVENT VENUE</p>
+						<p class="subtitle">Event venue location info</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="hero" id="booth">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-multiline">
+					<div class="column is-12 has-text-centered">
+						<p class="title">CAREER BOOTH</p>
+					</div>
+					<div class="column is-12">
+						<div class="tile is-ancestor">
+							<div class="tile is-parent">
+								<article class="tile is-child box">
+									<p class="title">Hello World</p>
+									<p class="subtitle">What is up?</p>
+								</article>
+							</div>
+							<div class="tile is-parent">
+								<article class="tile is-child box">
+									<p class="title">Foo</p>
+									<p class="subtitle">Bar</p>
+								</article>
+							</div>
+							<div class="tile is-parent">
+								<article class="tile is-child box">
+									<p class="title">Third column</p>
+									<p class="subtitle">With some content</p>
+									<div class="content">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque
+											tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+									</div>
+								</article>
+							</div>
+						</div>
+
+						<div class="tile is-ancestor">
+							<div class="tile is-vertical is-8">
+								<div class="tile">
+									<div class="tile is-parent is-vertical">
+										<article class="tile is-child box">
+											<p class="title">Vertical tiles</p>
+											<p class="subtitle">Top box</p>
+										</article>
+										<article class="tile is-child box">
+											<p class="title">Vertical tiles</p>
+											<p class="subtitle">Bottom box</p>
+										</article>
+									</div>
+									<div class="tile is-parent">
+										<article class="tile is-child box">
+											<p class="title">Middle box</p>
+											<p class="subtitle">With an image</p>
+											<figure class="image is-4by3">
+												<img src="https://bulma.io/images/placeholders/640x480.png">
+											</figure>
+										</article>
+									</div>
+								</div>
+								<div class="tile is-parent">
+									<article class="tile is-child box">
+										<p class="title">Wide column</p>
+										<p class="subtitle">Aligned with the right column</p>
+										<div class="content">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque
+												tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+										</div>
+									</article>
+								</div>
+							</div>
+							<div class="tile is-parent">
+								<article class="tile is-child box">
+									<div class="content">
+										<p class="title">Tall column</p>
+										<p class="subtitle">With even more content</p>
+										<div class="content">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at
+												pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut.
+												Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut
+												quam.</p>
+											<p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis.
+												Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin
+												felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit
+												amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat
+												tortor.</p>
+											<p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor
+												consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper
+												a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+										</div>
+									</div>
+								</article>
+							</div>
+						</div>
+
+						<div class="tile is-ancestor">
+							<div class="tile is-parent">
+								<article class="tile is-child box">
+									<p class="title">Side column</p>
+									<p class="subtitle">With some content</p>
+									<div class="content">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque
+											tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+									</div>
+								</article>
+							</div>
+							<div class="tile is-parent is-8">
+								<article class="tile is-child box">
+									<p class="title">Main column</p>
+									<p class="subtitle">With some content</p>
+									<div class="content">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque
+											tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+									</div>
+								</article>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="hero" id="partners">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-multiline">
+					<div class="column is-12 has-text-centered">
+						<p class="title">SPONSORS & PARTNERS</p>
+					</div>
+					<div class="column is-12">
+						<div class="columns is-multiline">
+							<div class="column">
+								<figure class="image">
+									<img src="https://www.ump.edu.my/img/logo-ump-tvet-1.png" alt="">
+								</figure>
+							</div><div class="column">
+									<figure class="image">
+										<img src="https://www.ump.edu.my/img/logo-ump-tvet-1.png" alt="">
+									</figure>
+								</div><div class="column">
+									<figure class="image">
+										<img src="https://www.ump.edu.my/img/logo-ump-tvet-1.png" alt="">
+									</figure>
+								</div><div class="column">
+									<figure class="image">
+										<img src="https://www.ump.edu.my/img/logo-ump-tvet-1.png" alt="">
+									</figure>
+								</div><div class="column">
+									<figure class="image">
+										<img src="https://www.ump.edu.my/img/logo-ump-tvet-1.png" alt="">
+									</figure>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="hero" id="contact">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns">
+					<div class="column is-12 has-text-centered">
+						<p class="title">CONTACT US</p>
 					</div>
 				</div>
 			</div>
@@ -112,5 +406,4 @@ const scrollToElement = (id, duration) => {
 
 .overlay-75 {
 	background-color: rgb(0, 0, 0, 0.75);
-}
-</style>
+}</style>
