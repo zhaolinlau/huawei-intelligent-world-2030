b1bf28136b2317f55feeb86fdab792d1c03685cb
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-09 06:30:29 +0800

    debug 3

--- components/footer.vue
+++ components/footer.vue
@@ -2,7 +2,8 @@
 	<footer class="footer">
 		<div class="content has-text-centered">
 			<p>
-				Copyright &COPY; {{ new Date().getFullYear() }} <strong>Huawei Open Day: Intelligent World 2030</strong>. All rights reserved.
+				Copyright &COPY; {{ new Date().getFullYear() }} <strong>Huawei Open Day: Intelligent World 2030</strong>. All
+				rights reserved.
 			</p>
 		</div>
 	</footer>
--- oruga.js
+++ oruga.js
@@ -2,7 +2,7 @@ import Oruga from "@oruga-ui/oruga-next";
 import { bulmaConfig } from "@oruga-ui/theme-bulma";
 import "@/assets/css/bulma.css";
 import "@mdi/font/css/materialdesignicons.css";
-import "bulma-list/css/bulma-list.css"
+import "bulma-list/css/bulma-list.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.use(Oruga, bulmaConfig);
