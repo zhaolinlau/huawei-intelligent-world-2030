187dcbdf2f83216f7b92edbd0b7d91a178f4ea27
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-17 19:35:37 +0800

    revert ogImage

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ useSeoMeta({
 	ogTitle: 'Huawei Open Day: Intelligent World 2030',
 	description: "Huawei's Intelligent World 2030 vision aims to improve quality of life through technology and sustainability. It includes sustainable diets, comfortable living spaces, and eliminating city traffic congestion and pollution.",
 	ogDescription: "Huawei's Intelligent World 2030 vision aims to improve quality of life through technology and sustainability. It includes sustainable diets, comfortable living spaces, and eliminating city traffic congestion and pollution.",
-	ogImage: 'https://huawei-intelligent-world-2030.vercel.app/img/home.png',
+	ogImage: 'img/home.png',
 	twitterCard: 'summary_large_image',
 })
 </script>
