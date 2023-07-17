deb32fc8313fee0fb0d45f1aaadc4d9c841e0274
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-17 18:12:22 +0800

    minimize css

--- oruga.js
+++ oruga.js
@@ -1,7 +1,7 @@
 import Oruga from "@oruga-ui/oruga-next";
 import { bulmaConfig } from "@oruga-ui/theme-bulma";
-import "@/assets/css/bulma.css";
-import "@mdi/font/css/materialdesignicons.css";
+import "@/assets/css/bulma.min.css";
+import "@mdi/font/css/materialdesignicons.min.css";
 import "bulma-list/css/bulma-list.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
