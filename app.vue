09851d44ecab22987c1e2ef8b9c4729d25c50f8d
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-17 19:10:08 +0800

    add seo and head

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+useHead({
+	title: 'Huawei Open Day: Intelligent World 2030',
+	meta: [
+		{ name: 'description', content: "Huawei's Intelligent World 2030 vision aims to improve quality of life through technology and sustainability. It includes sustainable diets, comfortable living spaces, and eliminating city traffic congestion and pollution." }
+	]
+})
+
+useSeoMeta({
+	title: 'Huawei Open Day: Intelligent World 2030',
+	ogTitle: 'Huawei Open Day: Intelligent World 2030',
+	description: "Huawei's Intelligent World 2030 vision aims to improve quality of life through technology and sustainability. It includes sustainable diets, comfortable living spaces, and eliminating city traffic congestion and pollution.",
+	ogDescription: "Huawei's Intelligent World 2030 vision aims to improve quality of life through technology and sustainability. It includes sustainable diets, comfortable living spaces, and eliminating city traffic congestion and pollution.",
+	ogImage: '/img/home.png',
+	twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
-  <div>
-    <RouterView />
-  </div>
+	<div>
+		<RouterView />
+	</div>
 </template>
