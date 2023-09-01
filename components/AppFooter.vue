490b86dddcaa03d59d4abec9078513d954564dc2
Zhaolin Lau <zhaolinlau@gmail.com>
2023-09-01 23:05:54 +0800

    add license

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,8 +2,14 @@
 	<footer class="footer">
 		<div class="content has-text-centered">
 			<p>
-				Copyright &COPY; {{ new Date().getFullYear() }} <strong>Huawei Open Day: Intelligent World 2030</strong>. All
-				rights reserved.
+				Source code licensed under
+				<a class="has-text-primary" href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU GPLv3</a>.
+				<br>
+				Website content licensed under
+				<a class="has-text-primary" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA
+					4.0</a>.
+				<br>
+				Made by <a class="has-text-primary" href="https://zhaolinlau.vercel.app/" target="_blank">Zhaolin Lau</a>.
 			</p>
 		</div>
 	</footer>
