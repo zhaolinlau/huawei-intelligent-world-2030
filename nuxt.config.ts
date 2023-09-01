90760f50f20b250e7e554b133322c7414a066026
Zhaolin Lau <zhaolinlau@gmail.com>
2023-09-01 17:54:34 +0800

    redeploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	plugins: [{ src: "oruga.js" }],
-});
+  devtools: { enabled: true },
+	plugins: [{ src: "oruga.js" }]
+})
